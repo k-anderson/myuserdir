a5a144b8d5f0ee8364f8060ebb1357bfa189d4a3
karl anderson <karl@2600hz.com>
2012-10-28 07:17:23 -0700

    wmii config, xorg for my desktop displays, and cheat sheet for this round of re-installation

--- scripts/extreme_alert.sh
+++ scripts/extreme_alert.sh
@@ -5,17 +5,17 @@ trap "kill %1; killall play" EXIT
 set -e
 
 if `echo -n $1 | grep -qE "(RECOVERY|ACKNOWLEDGEMENT)"`; then
-	exit 0
+  exit 0
 fi
 
 while [ -z "`pgrep -f 'play /home/kanderson/media/audio/extreme_alert.wav'`" ]; do 
-	play /home/kanderson/media/audio/extreme_alert.wav; 
-done &
+  aplay /home/kanderson/media/audio/extreme_alert.wav &
+done 
 
-kdialog --warningyesno "Received new alert:\n$1\nAcknowledge?"
+zenity --question --text "Received new alert:\n$1\nAcknowledge?"
 
 if [ $? == 0 ]; then
-        /usr/bin/mail -s "test ack RE: $1" -r "karl@2600hz.com" "$2" <<EOF
+  /usr/bin/mail -s "test ack RE: $1" -r "karl@2600hz.com" "$2" <<EOF
 EOF
 fi
 
